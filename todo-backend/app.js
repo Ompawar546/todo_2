6ecec599e872fdc9e877cca35eab745657094550
om_pawar <ompawar3000@gmail.com>
2025-04-03 15:17:22 +0530

    CORS issue

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -5,7 +5,12 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://todo-2-hdy3.onrender.com', // Allow only your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
+
 
 app.use(express.json()); // convert req body into json
 
